c7d5f1e2afc94481015fa16733fe23542d631963
Mahkamov Ahadjon <ahadjonmahkamov111@gmail.com>
2023-08-13 13:48:43 +0500

    homepage

--- src/Components/Layout/Footer/Footer.jsx
+++ src/Components/Layout/Footer/Footer.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import './Footer.css';
+import { Link } from 'react-router-dom';
+
+// import footerfonimg from '../' 
 
 export default function Footer() {
   return (
     <>
-      <h1>
-        Bugunoq toplink jamoasiga qo'shiling 
-      </h1>
-      <a href="#"> Toplinkdan bepul fodalaning</a>
+     <div className="footer">
+      <div className="container">
+        <div className="footer-box">
+          <h2 className='footer-subtext'>Bugunoq Toplink jamoasiga qo'shiling</h2>
+          <p className="footer-text">O'z izdoshlariga o'zlarining barcha mazmunini kashf qilishda yordam berish uchun Toplink-dan foydalanadigan minglab foydalanuvchilarga qo'shiling.</p>
+          <Link to={"/"} className="footer-btn">Toplinkdan bepul foydalaning</Link>
+          <p className="footer-text">Bu haftada 5318 kishi roʻyxatdan oʻtgan!</p>
+          <div className='footer-link-box'>
+            <a className='footer-link' href="#">U qanday ishlaydi</a>
+            <a className='footer-link' href="#">Xususiyat</a>
+            <a className='footer-link' href="#">FAQ</a>
+            <a className='footer-link' href="#">Xizmat ko'rsatish shartlari</a>
+            <a className='footer-link' href="#">Maxfiylik siyosati</a>
+          </div>
+          <p className='footer-bottom-text'>Copyright © 2023 Toplink Barcha huquqlar himoyalangan</p>
+        </div>
+      </div>
+     </div>
     </>
   )
 }
